e57c2dd82780b9bbd884ffd496a1d9260c74898a
dvlpr1996 <nimajahanbakhshian@gmail.com>
2021-12-08 23:13:46 +0330

    implement libs/ helper.php

--- libs/helper.php
+++ libs/helper.php
@@ -3,14 +3,27 @@
 function nice_dd($data)
 {
 	echo "<pre style='color: #9c4100; background: #fff; z-index: 999; position: relative; padding: 10px; margin: 10px; border-radius: 5px; border-left: 3px solid #c56705;'>";
-	var_dump($data);
+	print_r($data);
 	echo "</pre>";
 	die();
 }
 
-function redirect(string $url)
+function redirect(string $path)
 {
-	header("Location: $url");
+	return header("Location:" . $path . ".php");
 	die();
 }
 
+function file_name($name)
+{
+	$name = (string)$name;
+	$name = strtolower($name);
+	$name = str_replace(" ", "-", $name);
+	return md5(time()) . "-" . $name;
+}
+
+function des_file($path, $name)
+{
+	$name = md5(time()) . "-" . $name;
+	return $path . $name;
+}
